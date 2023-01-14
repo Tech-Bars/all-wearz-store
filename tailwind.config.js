0e8bf1320260e9e15639c53c14da6165e558a8e3
eimaam <imamddahir@gmail.com>
2023-01-14 08:19:51 +0100

    feat: configured tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,54 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': {'max': '820px'},
+      'md': {'min': '820px', 'max': '1510px'},
+      'lg': "1510px",
+    },
+    extend: {
+      fontSize: {
+        "text-base": "0.9rem"
+      },
+      colors: {
+        'default': '#fff',
+        'primary': '#61DB98',
+        'secondary': '#ebfff5',
+        'dark': '#212121',
+        'bgColor': 'rgb(240, 239, 239)',
+        'grey': 'rgb(57, 56, 56)',
+        "extras": "#788fdb"
+      },
+      width: {
+        "200px": "200px",
+        "100px": "100px",
+        "600px": "600px",
+        '15': '15%',
+        '85': '85%',
+        '55': "55%",
+        '60': "60%",
+        '80': '80%',
+        '70': '70%',
+        '90': '90%',
+      },
+      height: {
+        '70vh': "70vh" 
+      }
+    },
+    fontFamily: {
+      "spaceGrotesk": ['"Space Grotesk", sans-serif']
+    },
+    borderRadius: {
+      'none': '0',
+      'sm': '0.125rem',
+      default: '0.25rem',
+      'md': '0.5rem',
+      'lg': '1rem',
+    }
+    
   },
   plugins: [],
 }
