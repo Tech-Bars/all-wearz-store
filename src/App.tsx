46dab5af74a5b679dce73a61c7968d33eb330694
eimaam <imamddahir@gmail.com>
2023-02-02 10:41:15 +0100

    feat: added login and signup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import { Signup } from './pages/Signup';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/' element={<Login />} />
+        <Route path='/' element={<Signup />} />
       </Routes>
     </>
   );
