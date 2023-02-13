e59f9a2da56ec0da3bd5febb2711559c14c3e42e
eimaam <imamddahir@gmail.com>
2023-02-13 22:46:33 +0100

    added cart route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
+import Cart from './pages/Cart';
 import { Home } from './pages/Home';
 import { Login } from './pages/Login';
 import { Signup } from './pages/Signup';
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/signin' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
+        <Route path='/cart' element={<Cart />} />
       </Routes>
       <Footer />
     </>
