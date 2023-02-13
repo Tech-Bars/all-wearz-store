ab322e4d3bb2e68970d5ed8741b566626015d4f7
eimaam <imamddahir@gmail.com>
2023-02-13 22:46:08 +0100

    feat: created cart page and component

---
+++ src/components/CartItem.tsx
@@ -0,0 +1,31 @@
+import { faTrashCan } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { PropsWithChildren } from 'react'
+import dummy from "../components/Home/assets/shirt.jpg";
+import { MdDeleteForever } from "react-icons/md"
+
+
+const CartItem: React.FC<PropsWithChildren> = () => {
+  return (
+    <div className='flex flex-wrap border-bottom border-b py-2 justify-between  gap-4 items-center'>
+        <div className='flex gap-4 items-center'>
+            <img src={dummy} alt="" className='w-32 h-auto'/>
+            <div className='leading-8'>
+                <h4>Beach Wear</h4>
+                <p><span className='text-gray-500'>Color:</span> Red</p>
+                <p className='cursor-pointer flex items-center'>
+                    <MdDeleteForever  className='text-lg text-red-600'/>
+                    &nbsp;Remove
+                </p>
+            </div>
+        </div>
+        <div className='flex flex-col gap-4'>
+            <h4>Quantity</h4>
+            <input type="number" max={50} placeholder='1' className='w-16 text-center '/>
+            <b>₦ 15,000</b>
+        </div>
+    </div>
+  )
+}
+
+export default CartItem;
