0a519fc941f8953e9e2f9e61abcff892909c90ce
eimaam <imamddahir@gmail.com>
2023-02-13 17:03:18 +0100

    fixed mobile navigation bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,7 +8,7 @@ export const Nav = () => {
     const [showNav, setShowNav] = useState<boolean>(false)
 
   return (
-    <div className='bg-primary flex justify-between items-center gap-4 px-4 lg:px-16 md:px-16 py-2'>
+    <div className='bg-primary z-50 flex justify-between items-center gap-4 px-4 lg:px-16 md:px-16 py-3'>
         <div>
             <h3 className='text-center'>ALL WEARZ LUXURY</h3>
         </div>
@@ -25,7 +25,7 @@ export const Nav = () => {
 
         {showNav 
         &&
-        <div className='flex flex-col justify-center items-center fixed md:relative lg:relative left-0 right-0 top-0 min-h-screen md:min-h-0 lg:min-h-0 bg-primary lg:flex-row md:flex-row gap-6'>
+        <div className='flex flex-col justify-center items-center fixed md:relative lg:relative left-0 right-0 top-0 min-h-screen md:min-h-0 lg:min-h-0 bg-primary z-50 lg:flex-row md:flex-row gap-6'>
             {/* close menu bar icon */}
             <h1 onClick={() => setShowNav(false)}>
                 <svg className='absolute top-5 right-5 md:hidden lg:hidden' width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -46,9 +46,8 @@ export const Nav = () => {
         </div>
         }
 
-        {!showNav 
-        &&
-        <div className='flex justify-center items-center gap-6'>
+        
+        <div className='hidden lg:md:flex md:flex justify-center items-center gap-6'>
             <Link to="/cart">
                 <img src={cart} alt="cart icon" />
             </Link>
@@ -56,7 +55,6 @@ export const Nav = () => {
                 <img src={avatar} alt="avatar" />
             </Link>
         </div>
-        }
 
         {/* show menu icon */}
         <div className='md:hidden lg:hidden' onClick={() => setShowNav(true)}>
