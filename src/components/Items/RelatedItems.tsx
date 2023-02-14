3f96fabf8e27d42af57a8efd986a15bd019776fa
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2023-02-14 19:04:54 +0800

    fixed typo in related products component

--- src/components/Items/RelatedItems.tsx
+++ src/components/Items/RelatedItems.tsx
@@ -4,7 +4,7 @@ import shirt from '../Home/assets/shirt.jpg'
 const RelatedItems:React.FC = () => {
   return (
     <div className="w-full flex flex-col gap-3">
-      <h2 className="text-black font-medium text-2xl">Related Porducts</h2>
+      <h2 className="text-black font-medium text-2xl">Related Products</h2>
         <div className="flex flex-row gap-5 overflow-x-scroll scroll-smooth scrollbar-hide">
           <img src={shirt} alt="shirt img" className="w-72 h-72 rounded-lg cursor-pointer"/>
           <img src={shirt} alt="shirt img" className="w-72 h-72  rounded-lg cursor-pointer"/>
