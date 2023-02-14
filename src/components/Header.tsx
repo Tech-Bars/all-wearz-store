8e0c06e34daf88c703a10a7355f7d54d2cd7a48f
eimaam <imamddahir@gmail.com>
2023-02-14 16:58:34 +0100

    styling: redesigned the header page and added more details of the store

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,15 @@ import React from 'react'
 export const Header = () => {
 
   return (
-    <div className='bg-headerImage bg-cover text-center bg bg-no-repeat bg-center h-[80vh] flex flex-col justify-center items-center'>
-        <h1 className='font-bold text-4xl px-4 lg:text-8xl  md:text-6xl text-default outline outline-primary  shadow-black drop-shadow-lg md:drop-shadow-[0_15px_8px_rgba(0,0,0,0.6)] lg:drop-shadow-[0_20px_10px_rgba(0,0,0,0.6)]'>
+    <div className='text-center h-[80vh] flex flex-col gap-2 justify-center items-center'>
+        <h1 className='font-bold text-4xl px-4 lg:text-8xl  md:text-6xl text-primary outline outline-primary  shadow-black drop-shadow-lg '>
           LIVE YOUR STYLE
         </h1>
-        <h2 className='text-2xl my-4 lg:text-4xl md:text-4xl tracking-normal md:tracking-widest lg:tracking-widest text-default drop-shadow-lg '><i>...endless wardrobe glamour</i></h2>
+        <h2 className='text-2xl my-4 lg:text-4xl md:text-4xl tracking-normal drop-shadow-lg text-grey'>
+          <i>...endless wardrobe glamour</i>
+        </h2>
+        <p>Exquisite Clothing | Shirts | Shoes | Jackets | Pants | Pantofle | Jewelries | Fashion Accessories & lots more.</p>
+        <button className='btn-primary w-max mt-10'>SHOP NOW!</button>
     </div>
   )
 }
