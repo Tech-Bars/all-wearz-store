fc296a1cafa9b88f85f5b0bdab9a4bd964e884b2
eimaam <imamddahir@gmail.com>
2023-02-15 20:15:33 +0100

    styling: reduced tagline size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
         <h1 className='font-bold text-4xl px-4 lg:text-8xl  md:text-6xl text-primary outline outline-primary  shadow-black drop-shadow-lg '>
           LIVE YOUR STYLE
         </h1>
-        <h2 className='text-2xl my-4 lg:text-4xl md:text-4xl tracking-normal drop-shadow-lg text-grey'>
+        <h2 className='text-xl my-2 lg:text-4xl md:text-4xl tracking-normal drop-shadow-lg text-grey'>
           <i>...endless wardrobe glamour</i>
         </h2>
         <p>Exquisite Clothing | Shirts | Shoes | Jackets | Pants | Pantofle | Jewelries | Fashion Accessories & lots more.</p>
