d84dac5be403cde82173aee9e43362edb358e200
eimaam <imamddahir@gmail.com>
2023-02-15 19:23:35 +0100

    styling: redesign

--- src/components/Home/Newarrivals.tsx
+++ src/components/Home/Newarrivals.tsx
@@ -4,18 +4,17 @@ import Vector from './assets/Vector.svg'
 
 const Newarrivals:React.FC = () => {
   return (
-    <div className="px-5 mb-20 flex w-full gap-5 justify-center bg-[#D9D9D9] h-[368px] items-center font-sora rounded-md">
-      
-      <div className=" w-2/5 flex-col flex gap-5">
-            <h1 className=" font-semibold text-4xl">New Arrivals</h1>
+    <div className="my-4 p-5 flex flex-col md:flex-row lg:flex-row w-full gap-5 justify-center bg-[#D9D9D9] min-h-[368px] items-center font-sora rounded-md m-auto">
+      <div className="w-full md:w-1/2 lg:w-1/2 flex-col flex gap-5 text-center items-center md:text-left lg:text-left">
+            <h2 className="text-dark font-semibold text-2xl lg:text-3xl md:text-3xl">New Arrivals</h2>
             <span className="font-medium text-2xl">Discover Amazing Deals</span>
             <button className="flex items-center gap-3 transition-all duration-300 delay-100 ease-in-out hover:scale-110 bg-blue py-3 px-5 text-base text-white rounded-sm w-[162px]">
               Shop Now 
-            <img src={Vector} alt="vector"  />
+            <img src={Vector} alt="vector" />
             </button>
       </div>
-      <div className=" ml-20 w-[500px] h-[360px]">
-            <img src={shirt} alt="" className="h-full w-full" />
+      <div className='w-1/2'>
+          <img src={shirt} alt="" className="h-auto lg:w-[80%] md:w-[80%] m-auto" />
       </div>
     </div>
   )
