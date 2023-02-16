26edab6dd7e5609fc28b2d42b0ab77d73d69c6bb
eimaam <imamddahir@gmail.com>
2023-02-16 10:34:46 +0100

    created mock data

---
+++ src/fakeData.ts
@@ -0,0 +1,68 @@
+import shirt from '../src/components/Home/assets/shirt.jpg'
+import shoe from '../src/components/Home/assets/shoe.jpg'
+
+export const shirtsList = [
+    {
+      image: shirt,
+      name: "Beach Wear",
+      price: 100,
+    },
+    {
+      image: shirt,
+      name: "Beach Wear",
+      price: 100,
+    },
+    {
+      image: shirt,
+      name: "Beach Wear",
+      price: 100,
+    },
+    {
+      image: shirt,
+      name: "Beach Wear",
+      price: 100,
+    },
+    {
+      image: shirt,
+      name: "Beach Wear",
+      price: 100,
+    },
+    {
+      image: shirt,
+      name: "Beach Wear",
+      price: 100,
+    },
+  ]
+
+  export const shoesList = [
+    {
+      image: shoe,
+      name: "Unbranded",
+      price: 100,
+    },
+    {
+      image: shoe,
+      name: "Unbranded",
+      price: 100,
+    },
+    {
+      image: shoe,
+      name: "Unbranded",
+      price: 100,
+    },
+    {
+      image: shoe,
+      name: "Unbranded",
+      price: 100,
+    },
+    {
+      image: shoe,
+      name: "Unbranded",
+      price: 100,
+    },
+    {
+      image: shoe,
+      name: "Unbranded",
+      price: 100,
+    },
+  ]
