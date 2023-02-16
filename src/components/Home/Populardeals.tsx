b60a1c52c6eb247343a22e0d26ecad7f504772c7
eimaam <imamddahir@gmail.com>
2023-02-16 09:06:24 +0100

    chore:Styling: redesigned the component

--- src/components/Home/Populardeals.tsx
+++ src/components/Home/Populardeals.tsx
@@ -5,19 +5,21 @@ import shoes from './assets/shoe.jpg'
 
 const Populardeals:React.FC = () => {
   return (
-    <div className="w-full flex gap-5 px-5 pb-20 justify-center rounded-lg relative">
-           <h1 className=" flex items-center justify-center text-primary  text-3xl bg-secondary font-semibold absolute w-[311px] h-[68px] right-36 top-60">Popular deals</h1>
-      <div className="w-[750px] h-[540px]">
-           <img src={shoes} alt="img" className="w-full h-full rounded-lg" />
-      </div>
-      <div className="flex flex-col gap-5 py-3">
-       <div className="w-[350px] h-[252px]">
-       <img src={shirt} alt="img" className="w-full h-full rounded-lg" />
-       </div>
-       <div className="w-[350px] h-[252px]">
-       <img src={pant} alt="img" className="w-full h-full border rounded-lg" />
-       </div>
-      </div>
+    <div className="w-full h-[30rem] flex gap-2 md:gap-5 lg:gap-5 md:px-5 lg:px-5 justify-center items-center rounded-lg relative">
+        <h1 className="p-2 md:p-4 rounded-md text-default text-lg lg:text-3xl md:text-3xl bg-primary md:font-semibold lg:font-semibold absolute right-[10%] top-[45%]">
+          Popular Deals
+        </h1>
+        <div className="md:w-1/2 lg:w-1/2 h-[20rem] md:h-[28rem] lg:h-[28rem]">
+          <img src={shoes} alt="img" className="w-full h-full" />
+        </div>
+        <div className="md:w-1/4 lg:w-1/4 flex flex-col justify-center gap-2">
+          <div className='h-[10rem] md:h-[14rem] lg:h-[14rem]'>
+            <img src={shirt} alt="img" className="h-full w-full" />
+          </div>
+          <div className='h-[10rem] md:h-[14rem] lg:h-[14rem]'>
+            <img src={pant} alt="img" className="h-full w-full" />
+          </div>
+        </div>
     </div>
   )
 }
