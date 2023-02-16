7d996d7d38d30d67d0349704b1467c92392e7f41
eimaam <imamddahir@gmail.com>
2023-02-16 11:11:53 +0100

    feat: added trousers data

--- src/fakeData.ts
+++ src/fakeData.ts
@@ -1,5 +1,6 @@
 import shirt from '../src/components/Home/assets/shirt.jpg'
 import shoe from '../src/components/Home/assets/shoe.jpg'
+import pant from '../src/components/Home/assets/pant.jpg'
 
 export const shirtsList = [
     {
@@ -66,3 +67,35 @@ export const shirtsList = [
       price: 100,
     },
   ]
+export const trousersList = [
+    {
+        image: pant,
+        name: "Botegga Pant",
+        price: 100,
+    },
+    {
+        image: pant,
+        name: "Botegga Pant",
+        price: 100,
+    },
+    {
+        image: pant,
+        name: "Botegga Pant",
+        price: 100,
+    },
+    {
+        image: pant,
+        name: "Botegga Pant",
+        price: 100,
+    },
+    {
+        image: pant,
+        name: "Botegga Pant",
+        price: 100,
+    },
+    {
+        image: pant,
+        name: "Botegga Pant",
+        price: 100,
+    },
+]
