45eaaa733677f4ddcac1810a925730cbb90f8523
eimaam <imamddahir@gmail.com>
2023-02-16 09:17:26 +0100

    resized images

--- src/components/Home/CategoryCard.tsx
+++ src/components/Home/CategoryCard.tsx
@@ -7,7 +7,7 @@ interface CategoryData {
 
 const CategoryCard:React.FC<CategoryData> = ({image, title}:CategoryData) => {
   return (
-    <div className="w-[20rem] flex flex-col justify-center items-center gap-2 h-[14.6rem] shrink-0">
+    <div className="w-[10rem] md:w-[18rem] lg:w-[18rem] h-auto flex flex-col justify-center items-center gap-2 shrink-0">
         <img src={image} alt="shirt" className="h-[85%] w-full" />
         <h3 className="font-bold uppercase">{title}</h3>
     </div>
