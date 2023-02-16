942671bf82207d47f336450683791793c5a40760
eimaam <imamddahir@gmail.com>
2023-02-16 10:15:34 +0100

    style: fixed section height

--- src/components/Home/Newarrivals.tsx
+++ src/components/Home/Newarrivals.tsx
@@ -4,7 +4,7 @@ import Vector from './assets/Vector.svg'
 
 const Newarrivals:React.FC = () => {
   return (
-    <div className="my-4 p-5 flex flex-col md:flex-row lg:flex-row w-full gap-5 justify-center bg-[#D9D9D9] h-[368px] items-center font-sora rounded-md m-auto">
+    <div className="my-4 p-5 md:h-[368px] lg:h-[368px] flex flex-col md:flex-row lg:flex-row w-full gap-5 justify-center bg-[#D9D9D9] items-center font-sora rounded-md m-auto">
       <div className="w-full md:w-1/2 lg:w-1/2 flex-col flex gap-5 text-center items-center md:text-left lg:text-left">
             <h2 className="text-dark font-semibold text-2xl lg:text-3xl md:text-3xl">New Arrivals</h2>
             <span className="font-medium text-2xl">Discover Amazing Deals</span>
